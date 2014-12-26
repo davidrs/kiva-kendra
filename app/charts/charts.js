ec900b88efafa760a5b1d221a106d544809a832d
davidrs <david.rs@hotmail.com>
2014-12-26 01:33:30 -0500

    Added more charts very dirty right now

--- app/charts/charts.js
+++ app/charts/charts.js
@@ -10,48 +10,72 @@ angular.module('myApp.charts', ['ngRoute','myApp.recent','angularCharts'])
 }])
 
 .controller('ChartsCtrl',['$scope', 'Loans', function($scope, Loans) {
-  var convertLoansToPieChartData = function(data, xKey, yKey){
+
+console.log('runController');
+
+  var convertLoansToPieChartData = function(data, xKey, yKey, yKey2){
     var formattedData = {
-      series: ["Loans"],
+      series: ["loan_amount", "funded_amount"],
       data: []
     };
 
     // Aggregate to yKey to xKey.
-    data = _(data).groupBy(function(d){return d.location.country;});
+    if(data[0][xKey] && typeof data[0][xKey] !=  'object'){
+      data = _(data).groupBy(function(d){return d[xKey];});
+    } else if (xKey == 'country') {
+      data = _(data).groupBy(function(d){return d.location.country;});
+    }
+
+
     _(data).map(function(value, key){
-      console.log('value, key', value,  key);
-      value = _(value).reduce(function(memo, value){ return memo + value[yKey]; }, 0);
+      var y = [];
+      y[0] = _(value).reduce(function(memo, value){ return memo + value[yKey]; }, 0);
+      if(yKey2){
+        y[1] = _(value).reduce(function(memo, value){ return memo + value[yKey2]; }, 0);
+      }
+
       formattedData.data.push({
         x: key,
-        y: [value],
-        tooltip: key + ': $' + value
+        y: y,
+        tooltip: key + ' ratio: %' + Math.round(100*y[1]/y[0])
       });
+    });
+
+    formattedData.data = _(formattedData.data).sortBy(function(data){
+      return data.y[0];
     });
 
     console.log('formattedData', formattedData);
     return formattedData;
   };
 
-  Loans.getLoans().then(function(loans) {
-    $scope.data = convertLoansToPieChartData(loans, 'country', 'loan_amount');
-  });
-
-  $scope.chartType = 'pie';
-
-  $scope.data = {
-    series: ["Sales", "Income", "Expense"],
-    data: [{
-      x: "Computers",
-      y: [54, 0, 879],
-      tooltip: "This is a tooltip"
-    }]
-  };
+  setInterval(function(){
+    Loans.getLoans().then(function(loans) {
+      $scope.data = convertLoansToPieChartData(loans, 'country', 'loan_amount', 'funded_amount');
+      $scope.data2 = convertLoansToPieChartData(loans, 'sector', 'loan_amount', 'funded_amount');
+      $scope.data3 = convertLoansToPieChartData(loans, 'loan_amount', 'loan_amount', 'funded_amount');
+      $scope.data4 = convertLoansToPieChartData(loans, 'partner_name', 'loan_amount', 'funded_amount');
 
 
+      var minDate = new Date(_.min(loans, function(loan){
+        return Date.parse(loan.posted_date);
+      }).posted_date).toDateString();
+      var maxDate = new Date(_.max(loans, function(loan){
+        return Date.parse(loan.posted_date);
+      }).posted_date).toDateString();
+
+      $scope.config.title = "country from: " + maxDate +" to " +minDate;
+      $scope.config2.title = "sector from: " + maxDate +" to " +minDate
+      $scope.config3.title = "loan_amount from: " + maxDate +" to " +minDate
+      $scope.config4.title = "partner_id from: " + maxDate +" to " +minDate
+    });
+  }, 3000);
+
+  $scope.chartType = 'bar';
   $scope.config = {
     title: 'Recent Loan Listings', // chart title
     tooltips: true,
-    labels: true, // labels on data points
+    labels: false, // labels on data points
     // exposed events
     mouseover: function() {},
     mouseout: function() {},
@@ -60,18 +84,19 @@ angular.module('myApp.charts', ['ngRoute','myApp.recent','angularCharts'])
     legend: {
       display: true, // can be either 'left' or 'right'.
       position: 'right',
-      // you can have html in series name
-      htmlEnabled: false
     },
     // override this array if you're not happy with default colors
     colors: [],
     innerRadius: 10, // Only on pie Charts
     lineLegend: 'lineEnd', // Only on line Charts
     lineCurveType: 'cardinal', // change this as per d3 guidelines to avoid smoothline
-    isAnimate: true, // run animations while rendering chart
+    isAnimate: false, // run animations while rendering chart
     yAxisTickFormat: 's', //refer tickFormats in d3 to edit this value
     xAxisMaxTicks: 7 // Optional: maximum number of X axis ticks to show if data points exceed this number
   };
+  $scope.config2 = _.clone($scope.config);
+  $scope.config3 = _.clone($scope.config);
+  $scope.config4 = _.clone($scope.config);
 
 
 }]);
