8af966a626611cd12f40352842f55caa0a084785
davidrs <david.rs@hotmail.com>
2015-01-04 23:40:19 -0500

    Refactored to be much cleaner. Added some styles. Added sort option

--- app/charts/charts.js
+++ app/charts/charts.js
@@ -1,5 +1,29 @@
 'use strict';
 
+var chartSettings = [{
+    value: 'country',
+    yValues: [ 'loan_amount', 'funded_amount']
+  },{
+    value: 'country',
+    yValues: ['borrower_count']
+  },{
+    value: 'sector',
+    yValues: [ 'loan_amount', 'funded_amount']
+  },{
+    value: 'partner_name',
+    yValues: [ 'loan_amount', 'funded_amount']
+  },{
+    value: 'loan_amount',
+    yValues: [ 'loan_amount', 'funded_amount']
+  },{
+    value: 'borrower_count',
+    yValues: [ 'loan_amount', 'funded_amount']
+  },{
+    value: 'status',
+    yValues: [ 'loan_amount', 'funded_amount']
+  }
+];
+
 angular.module('myApp.charts', ['ngRoute','myApp.recent','angularCharts'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -10,93 +34,105 @@ angular.module('myApp.charts', ['ngRoute','myApp.recent','angularCharts'])
 }])
 
 .controller('ChartsCtrl',['$scope', 'Loans', function($scope, Loans) {
+    $scope.data = [];
+    $scope.config = [];
 
-console.log('runController');
+    $scope.selectedSort = 'newest';
+    $scope.sortOptions = ['newest', 'popularity', 'loan_amount', 'expiration', 'oldest', 'amount_remaining', 'repayment_term', 'random'];
 
-  var convertLoansToPieChartData = function(data, xKey, yKey, yKey2){
-    var formattedData = {
-      series: ["loan_amount", "funded_amount"],
-      data: []
+    $scope.search = function(){
+      $scope.fetching = true;
+      Loans.getNewLoans($scope.selectedSort).then(function(loans) {
+        var dateRange = getDateRange(loans);
+
+        _(chartSettings).each(function(chartSetting, k){
+          $scope.data[k] = convertLoansToPieChartData(loans, chartSetting.value, chartSetting.yValues);
+
+          $scope.config[k] = _.clone(baseConfig);
+          $scope.config[k].title =  chartSetting.value + " from: " + dateRange.min +" to " + dateRange.max;
+          $scope.fetching = false;
+        });
+      });
+
+      $scope.chartType = 'bar';
+
+      var baseConfig = {
+        title: 'Recent Loan Listings', // chart title
+        tooltips: true,
+        labels: false, // labels on data points
+        // legend config
+        legend: {
+          display: true, // can be either 'left' or 'right'.
+          position: 'right',
+        },
+        // override this array if you're not happy with default colors
+        colors: [],
+        innerRadius: 10,        // Only on pie Charts
+        lineLegend: 'lineEnd',  // Only on line Charts
+        lineCurveType: 'cardinal', // change this as per d3 guidelines to avoid smoothline
+        isAnimate: false,        // run animations while rendering chart
+        yAxisTickFormat: 's', //refer tickFormats in d3 to edit this value
+        xAxisMaxTicks: 7 // Optional: maximum number of X axis ticks to show if data points exceed this number
+      };
     };
 
-    // Aggregate to yKey to xKey.
-    if(data[0][xKey] && typeof data[0][xKey] !=  'object'){
-      data = _(data).groupBy(function(d){return d[xKey];});
-    } else if (xKey == 'country') {
-      data = _(data).groupBy(function(d){return d.location.country;});
-    }
-
-
-    _(data).map(function(value, key){
-      var y = [];
-      y[0] = _(value).reduce(function(memo, value){ return memo + value[yKey]; }, 0);
-      if(yKey2){
-        y[1] = _(value).reduce(function(memo, value){ return memo + value[yKey2]; }, 0);
-      }
-
-      formattedData.data.push({
-        x: key,
-        y: y,
-        tooltip: key + ' ratio: %' + Math.round(100*y[1]/y[0])
-      });
-    });
-
-    formattedData.data = _(formattedData.data).sortBy(function(data){
-      return data.y[0];
-    });
-
-    console.log('formattedData', formattedData);
-    return formattedData;
-  };
-
-  setInterval(function(){
-    Loans.getLoans().then(function(loans) {
-      $scope.data = convertLoansToPieChartData(loans, 'country', 'loan_amount', 'funded_amount');
-      $scope.data2 = convertLoansToPieChartData(loans, 'sector', 'loan_amount', 'funded_amount');
-      $scope.data3 = convertLoansToPieChartData(loans, 'loan_amount', 'loan_amount', 'funded_amount');
-      $scope.data4 = convertLoansToPieChartData(loans, 'partner_name', 'loan_amount', 'funded_amount');
-
-
-      var minDate = new Date(_.min(loans, function(loan){
-        return Date.parse(loan.posted_date);
-      }).posted_date).toDateString();
-      var maxDate = new Date(_.max(loans, function(loan){
-        return Date.parse(loan.posted_date);
-      }).posted_date).toDateString();
-
-      $scope.config.title = "country from: " + maxDate +" to " +minDate;
-      $scope.config2.title = "sector from: " + maxDate +" to " +minDate
-      $scope.config3.title = "loan_amount from: " + maxDate +" to " +minDate
-      $scope.config4.title = "partner_id from: " + maxDate +" to " +minDate
-    });
-  }, 3000);
-
-  $scope.chartType = 'bar';
-  $scope.config = {
-    title: 'Recent Loan Listings', // chart title
-    tooltips: true,
-    labels: false, // labels on data points
-    // exposed events
-    mouseover: function() {},
-    mouseout: function() {},
-    click: function() {},
-    // legend config
-    legend: {
-      display: true, // can be either 'left' or 'right'.
-      position: 'right',
-    },
-    // override this array if you're not happy with default colors
-    colors: [],
-    innerRadius: 10, // Only on pie Charts
-    lineLegend: 'lineEnd', // Only on line Charts
-    lineCurveType: 'cardinal', // change this as per d3 guidelines to avoid smoothline
-    isAnimate: false, // run animations while rendering chart
-    yAxisTickFormat: 's', //refer tickFormats in d3 to edit this value
-    xAxisMaxTicks: 7 // Optional: maximum number of X axis ticks to show if data points exceed this number
-  };
-  $scope.config2 = _.clone($scope.config);
-  $scope.config3 = _.clone($scope.config);
-  $scope.config4 = _.clone($scope.config);
-
+    $scope.search();
 
 }]);
+
+var getDateRange = function(loans){
+  var dateRange = {min:null, max: null};
+
+  dateRange.min = new Date(_.min(loans, function(loan){
+    return Date.parse(loan.posted_date);
+  }).posted_date).toDateString();
+
+  dateRange.max = new Date(_.max(loans, function(loan){
+    return Date.parse(loan.posted_date);
+  }).posted_date).toDateString();
+
+  return dateRange;
+};
+
+var convertLoansToPieChartData = function(data, xKey, yKeys){
+  var formattedData = {
+    series: yKeys,
+    data: []
+  };
+
+  // Aggregate to yKey to xKey.
+  if(data[0][xKey] && typeof data[0][xKey] !=  'object'){
+    data = _(data).groupBy(function(d){return d[xKey];});
+  } else if (xKey == 'country') {
+    data = _(data).groupBy(function(d){return d.location.country;});
+  }
+
+
+  _(data).map(function(value, key){
+    var y = [];
+
+    _(yKeys).each(function(yKey){
+      y.push(_(value).reduce(function(memo, value){ return memo + value[yKey]; }, 0));
+    });
+
+    formattedData.data.push({
+      x: key,
+      y: y,
+      tooltip: key + (y[1]? ' ratio: %' + Math.round(100 * y[1]/y[0]) : ' '+ y[0])
+    });
+  });
+
+
+  // Sort data for chart.
+  // If the x value is an integer, sort by that, else sort by y value.
+  formattedData.data = _(formattedData.data).sortBy(function(data){
+    if(isNaN(data.x)){
+      return data.y[0];
+    } else{
+      return parseInt(data.x);
+    }
+  });
+
+  return formattedData;
+};
+
